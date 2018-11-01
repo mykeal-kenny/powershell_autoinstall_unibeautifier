f41e810e6401fd8d92dbfb09672c333e57faef16
FireInjun <mk@FireInjun.com>
2018-11-01 18:46:10 -0400

    11/1/2018 6:46 PM

--- Unibeautify_windows_beautifier_global_install.ps1
+++ Unibeautify_windows_beautifier_global_install.ps1
@@ -1,46 +1,46 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 function beautifier_install {
-# Uncomment the line below to install node as well
-# choco install nodejs
-#
-# Unibeautify-cli: the main Unibeautify package
-npm install --global unibeautify-cli
-#
-# Black: for Python beautification
-npm install --global @unibeautify/beautifier-black
-pip install black
-#
-# JS-Beautify
-npm install --global js-beautify
-npm install --global @unibeautify/beautifier-js-beautify
-#
-# Prettier
-npm install --global prettier
-npm install --global @unibeautify/beautifier-prettier
-#
-# Pretty Diff
-npm install --global prettydiff2
-npm install --global @unibeautify/beautifier-prettydiff
-#
-#NOTE: Everything below this line requires additional installs
-# If possible, there are choco installs to add the additional
-# required dependencies. To use, uncomment as needed.
-# =============================================================
-# 
-# ========Requires Visual Studio (NOT VS CODE)========
-# ClangFormat: For C, C++ and many others
-# npm install --global @unibeautify/beautifier-clang-format
-# choco install llvm
-#
-# Gofmt: For the Go language
-# npm install --global @unibeautify/beautifier-gofmt
-# choco install golang
-#
-# PHP-CS-Fixer
-# npm install --global @unibeautify/beautifier-php-cs-fixer
-#
-# PHP_CodeSniffer
-# npm install --global @unibeautify/beautifier-php-codesniffer
+  # Uncomment the line below to install node as well
+  # choco install nodejs
+  #
+  # Unibeautify-cli: the main Unibeautify package
+  npm install --global unibeautify-cli
+  #
+  # Black: for Python beautification
+  npm install --global @unibeautify/beautifier-black
+  pip install black
+  #
+  # JS-Beautify
+  npm install --global js-beautify
+  npm install --global @unibeautify/beautifier-js-beautify
+  #
+  # Prettier
+  npm install --global prettier
+  npm install --global @unibeautify/beautifier-prettier
+  #
+  # Pretty Diff
+  npm install --global prettydiff2
+  npm install --global @unibeautify/beautifier-prettydiff
+  #
+  #NOTE: Everything below this line requires additional installs
+  # If possible, there are choco installs to add the additional
+  # required dependencies. To use, uncomment as needed.
+  # =============================================================
+  # 
+  # ========Requires Visual Studio (NOT VS CODE)========
+  # ClangFormat: For C, C++ and many others
+  # npm install --global @unibeautify/beautifier-clang-format
+  # choco install llvm
+  #
+  # Gofmt: For the Go language
+  # npm install --global @unibeautify/beautifier-gofmt
+  # choco install golang
+  #
+  # PHP-CS-Fixer
+  # npm install --global @unibeautify/beautifier-php-cs-fixer
+  #
+  # PHP_CodeSniffer
+  # npm install --global @unibeautify/beautifier-php-codesniffer
 }
 
 beautifier_install
